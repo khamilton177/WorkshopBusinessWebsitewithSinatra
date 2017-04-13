5c0d7b26615fbcfe520c0813ae16b670713f1316
KHamilton177 <KHamilton177@verizon.net>
2017-04-12 22:29:31 -0400

    contact form elements.  Start of js file.  More layout and home css changes

---
+++ public/beauty.js
@@ -0,0 +1,3 @@
+var selectSub = document.querySelector("#subject");
+var selectSubVal = selectSub.options[selectSub.selectedIndex].value;
+var selectSubTxt = selectSub.options[selectSub.selectedIndex].text;
